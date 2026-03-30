863eb75fbcc2f72b53850a5e51afae3957ad952f
ankita5678992 <padhyankita387@gmail.com>
2026-03-30 20:29:13 +0530

    Add files via upload

---
+++ ppml extra question/numpy questions.py/numpy q3.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# Array with zeros
+a = np.zeros((2,3))
+
+# Array with ones
+b = np.ones((2,3))
+
+# Empty array
+c = np.empty((2,3))
+
+print("Array using zeros:")
+print(a)
+
+print("\nArray using ones:")
+print(b)
+
+print("\nArray using empty:")
+print(c)
